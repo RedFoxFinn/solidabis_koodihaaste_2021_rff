b81853ef170fc2d761add26a28711bab2bfd4400
RedFoxFinn <laborantti.antti@gmail.com>
2021-05-20 19:03:26 +0300

    feat: added instructions of the challenge (task.jsx)
    challenge instructions added to project: task, returning and evaluation

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Placeholder = () => <p>Solidabis Koodihaaste 2021</p>;
+
+const App = (props) => {
+  return <article id={props.appId}>
+  </article>
+};
+
+export default App;
