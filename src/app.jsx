08204e6d7967a8442e6fc48e685635fc8490d25a
RedFoxFinn <laborantti.antti@gmail.com>
2021-05-20 20:40:35 +0300

    feat: added css-files, app- & component-files, modified index.html

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useReducer} from 'react';
+import {
+  HashRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
 
-const Placeholder = () => <p>Solidabis Koodihaaste 2021</p>;
+import './styles/components.css';
+
+const Header = (props) => <article className='header'>
+  <p>{
+    props.lang === 'en'
+      ? `Solidabis code challenge 2021`
+      : `Solidabiksen koodihaaste 2021`
+  }</p>
+  <p>{props.lang === 'en' ? 'Author: ' : 'Tekijä: '}{
+    process.env.NODE_ENV === 'production'
+      ? `RedFoxFinn`
+      : `[RedFoxFinn]`
+  }</p>
+</article>;
 
 const App = (props) => {
-  return <article id={props.appId}>
-  </article>
+  const [lang, setLang] = useReducer('fi');
+  return <Router>
+    <Header lang={lang}/>
+    <Switch></Switch>
+  </Router>
 };
 
 export default App;
