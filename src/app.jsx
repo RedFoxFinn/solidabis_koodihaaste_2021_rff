2a298dd8f096887317a0269dbffdaec14c0bb9c3
RedFoxFinn <laborantti.antti@gmail.com>
2021-05-26 13:32:06 +0300

    feat: added custom react hook to follow window size

--- src/app.jsx
+++ src/app.jsx
@@ -13,8 +13,10 @@ import './styles/components.css';
 import './styles/global.css';
 import Navigation from './components/navigation';
 import Calculator from './components/calculator';
+import useWindowDimensions from './hooks/windowDimensionsHook';
 
 const App = (props) => {
+  const { height, width } = useWindowDimensions();
   const [lang, setLang] = useState();
   useEffect(() => {
     const selection = language.checkLang();
@@ -25,16 +27,18 @@ const App = (props) => {
     }
   },[]);
 
-  return <Router id={`${props.id}`}>
-    <CalculatorProvider>
-      <Header language={lang} authorName={packageinfo.author.name} authorUrl={packageinfo.author.url} />
-      <Navigation language={lang}/>
-      <Switch>
-        <Route exact path='/' children={<React.Fragment><Calculator/></React.Fragment>} />
-        <Route path='/task' children={<React.Fragment><h1>task</h1></React.Fragment>} />
-      </Switch>
-    </CalculatorProvider>
-  </Router>;
+  return <article style={{height: height, width: width, marginLeft: '1em', marginRight: '1em'}}>
+    <Router id={`${props.id}`}>
+      <CalculatorProvider>
+        <Header language={lang} authorName={packageinfo.author.name} authorUrl={packageinfo.author.url} />
+        <Navigation language={lang}/>
+        <Switch>
+          <Route exact path='/' children={<React.Fragment><Calculator/></React.Fragment>} />
+          <Route path='/task' children={<React.Fragment><h1>task</h1></React.Fragment>} />
+        </Switch>
+      </CalculatorProvider>
+    </Router>
+  </article>;
 };
 
 export default App;
